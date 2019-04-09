6e6fbe3b251708cda43f515aa09b4987e3025e02
marcelo <marcelosalles@github.com>
2019-04-09 14:50:22 -0300

    change seed single name

--- idf_bundler/idf_bundler.py
+++ idf_bundler/idf_bundler.py
@@ -20,18 +20,18 @@ def idf_bundler(input_files, output_name):
 ## EXEMPLES ##
 
 # DOUBLE, TIJOLO
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor1_single.txt','construction_tijolo.txt'],output_name='double_floor1_roof1_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor0_single.txt','construction_tijolo.txt'],output_name='double_floor0_roof1_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor1_single.txt','construction_tijolo.txt'],output_name='double_floor1_roof0_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor0_single.txt','construction_tijolo.txt'],output_name='double_floor0_roof0_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','pilotis_single.txt','construction_tijolo.txt'],output_name='double_pilotis_roof0_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor1_single.txt','construction_tijolo.txt'],output_name='double_floor1_roof1_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor0_single.txt','construction_tijolo.txt'],output_name='double_floor0_roof1_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor1_single.txt','construction_tijolo.txt'],output_name='double_floor1_roof0_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor0_single.txt','construction_tijolo.txt'],output_name='double_floor0_roof0_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','pilotis_single.txt','construction_tijolo.txt'],output_name='double_pilotis_roof0_tijolo.idf')
 
 # HIVE, TIJOLO
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor1_roof1_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor0_roof1_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor1_roof0_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor0_roof0_tijolo.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','pilotis_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_pilotis_roof0_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor1_roof1_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor0_roof1_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor1_roof0_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_floor0_roof0_tijolo.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','pilotis_single_hive.txt','crack.txt','construction_tijolo.txt'],output_name='hive_pilotis_roof0_tijolo.idf')
 
 # REF
 idf_bundler(input_files=['seed.txt','geom_ref.txt','sch_original.txt','roof1_ref.txt','floor1_ref.txt'],output_name='ref_floor1_roof1.idf')
@@ -48,16 +48,16 @@ idf_bundler(input_files=['seed.txt','geom_ref_adapted.txt','sch_original_adapted
 idf_bundler(input_files=['seed.txt','geom_ref_adapted.txt','sch_original_adapted.txt','roof0_adapted.txt','pilotis_adapted.txt'],output_name='adapted_pilotis_roof0.idf')
 
 # DOUBLE
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor1_single.txt','construction_concreto.txt'],output_name='double_floor1_roof1.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor0_single.txt','construction_concreto.txt'],output_name='double_floor0_roof1.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor1_single.txt','construction_concreto.txt'],output_name='double_floor1_roof0.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor0_single.txt','construction_concreto.txt'],output_name='double_floor0_roof0.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','pilotis_single.txt','construction_concreto.txt'],output_name='double_pilotis_roof0.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor1_single.txt','construction_concreto.txt'],output_name='double_floor1_roof1.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof1_single.txt','floor0_single.txt','construction_concreto.txt'],output_name='double_floor0_roof1.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor1_single.txt','construction_concreto.txt'],output_name='double_floor1_roof0.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','floor0_single.txt','construction_concreto.txt'],output_name='double_floor0_roof0.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_double.txt','sch_original_out_single.txt','roof0_single.txt','pilotis_single.txt','construction_concreto.txt'],output_name='double_pilotis_roof0.idf')
 
 # HIVE
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor1_roof1.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor0_roof1.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor1_roof0.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor0_roof0.idf')
-idf_bundler(input_files=['seed_upper.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','pilotis_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_pilotis_roof0.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor1_roof1.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof1_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor0_roof1.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor1_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor1_roof0.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','floor0_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_floor0_roof0.idf')
+idf_bundler(input_files=['seed_single.txt','geom_single_hive.txt','sch_original_adi_single.txt','roof0_single_hive.txt','pilotis_single_hive.txt','crack.txt','construction_concreto.txt'],output_name='hive_pilotis_roof0.idf')
 '''
