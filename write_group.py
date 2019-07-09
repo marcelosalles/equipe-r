65f47576c5fcb454cf847d8d4a21ba871ecefcd8
fagnerz <41924540+fagnerz@users.noreply.github.com>
2019-07-09 14:55:02 -0300

    Add files via upload
    Mudanças para leitura de múltiplos arquivos EPW.

--- write_group.py
+++ write_group.py
@@ -1,18 +1,18 @@
   # creates a e+ group file with all the idf models and epw files in folder.
 
-  import glob
-  import os
+import glob
+import os
 
-  listIDF = glob.glob("*.idf")
-  epws = glob.glob("*.epw")
+listIDF = glob.glob("*.idf")
+epws = glob.glob("*.epw")
 
-  conteudo = ""
-  for i in listIDF:
-      for epw in epws:
-          conteudo = conteudo + os.getcwd()+"\\"+i+","+os.getcwd()+"\\"+epw+","+os.getcwd()+"\\"+i[:-4]+",1\n"
+conteudo = ""
+for i in listIDF:
+  for epw in epws:
+      conteudo = conteudo + os.getcwd()+"\\"+i+","+os.getcwd()+"\\"+epw+","+os.getcwd()+"\\"+i[:-4]+",1\n"
 
-  posicao = os.getcwd().rfind("\\")+1
-  nome = os.getcwd()[posicao:]
+posicao = os.getcwd().rfind("\\")+1
+nome = os.getcwd()[posicao:]
 
-  with open(nome+"_group.epg", "w") as file:
-  	file.write(conteudo)
+with open(nome+"_group.epg", "w") as file:
+    file.write(conteudo)
