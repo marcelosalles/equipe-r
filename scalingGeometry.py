007e047eb45ecb112b95107138384424bb345ea9
marcelo <marcelosalles@github.com>
2019-11-21 13:10:05 -0300

    ScalingGeomtery with fixed openings

--- scalingGeometry.py
+++ scalingGeometry.py
@@ -1,5 +1,16 @@
 import json
 
+def constant_vertex(vertex, vertex_list, scale):
+    # Checks which vertex it is, so it defines the new value,
+    # in order to keep the same opening area
+
+    if vertex == min(vertex_list):
+        new_vertex = ((max(vertex_list)-min(vertex_list))*(scale-1))
+    elif vertex == max(vertex_list):
+        new_vertex = ((max(vertex_list)-min(vertex_list))*(scale+1))
+
+    return(new_vertex)
+
 def scaling(scalex=1.55, scaley=1.55,scalez=1.55,
     ratio_of_building_afn=0.85, window_scale=True, shading_scale=False,
     input_file='model.epJSON',output_name='scaled_model.epJSON'):
@@ -53,6 +64,38 @@ def scaling(scalex=1.55, scaley=1.55,scalez=1.55,
             content["FenestrationSurface:Detailed"][i]["vertex_3_z_coordinate"] = content["FenestrationSurface:Detailed"][i]["vertex_3_z_coordinate"]*scalez
             content["FenestrationSurface:Detailed"][i]["vertex_4_z_coordinate"] = content["FenestrationSurface:Detailed"][i]["vertex_4_z_coordinate"]*scalez
 
+    else:
+        for i in list(content["FenestrationSurface:Detailed"].keys()):
+
+            x_vertex = []
+            y_vertex = []
+            z_vertex = []
+
+            for vertex in list(content["FenestrationSurface:Detailed"][i].keys()):
+
+                if '_x_' in vertex:
+                    x_vertex.append(content["FenestrationSurface:Detailed"][i][vertex])
+                elif '_y_' in vertex:
+                    y_vertex.append(content["FenestrationSurface:Detailed"][i][vertex])
+                elif '_z_' in vertex:
+                    z_vertex.append(content["FenestrationSurface:Detailed"][i][vertex])
+            
+            content["FenestrationSurface:Detailed"][i]["vertex_1_x_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_1_x_coordinate"],x_vertex,scalex)
+            content["FenestrationSurface:Detailed"][i]["vertex_2_x_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_2_x_coordinate"],x_vertex,scalex)
+            content["FenestrationSurface:Detailed"][i]["vertex_3_x_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_3_x_coordinate"],x_vertex,scalex)
+            content["FenestrationSurface:Detailed"][i]["vertex_4_x_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_4_x_coordinate"],x_vertex,scalex)
+                
+            content["FenestrationSurface:Detailed"][i]["vertex_1_y_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_1_y_coordinate"],y_vertex,scaley)
+            content["FenestrationSurface:Detailed"][i]["vertex_2_y_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_2_y_coordinate"],y_vertex,scaley)
+            content["FenestrationSurface:Detailed"][i]["vertex_3_y_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_3_y_coordinate"],y_vertex,scaley)
+            content["FenestrationSurface:Detailed"][i]["vertex_4_y_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_4_y_coordinate"],y_vertex,scaley)
+
+            content["FenestrationSurface:Detailed"][i]["vertex_1_z_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_1_z_coordinate"],z_vertex,scalez)
+            content["FenestrationSurface:Detailed"][i]["vertex_2_z_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_2_z_coordinate"],z_vertex,scalez)
+            content["FenestrationSurface:Detailed"][i]["vertex_3_z_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_3_z_coordinate"],z_vertex,scalez)
+            content["FenestrationSurface:Detailed"][i]["vertex_4_z_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_4_z_coordinate"],z_vertex,scalez)
+
+
     ## Shading:Building:Detailed
     if shading_scale:
         for i in list(content["Shading:Building:Detailed"].keys()):
@@ -77,9 +120,9 @@ def scaling(scalex=1.55, scaley=1.55,scalez=1.55,
 ## Test function changing values on the following lines: ---------------
 
 # Define the name of the input file here
-FILE_NAME = 'example.epJSON'
+FILE_NAME = 'singlezone_gen/hive_12-04_floor0_roof0.epJSON'
 
 # Define the input parameters here
 scaling(scalex=2, scaley=.5,scalez=1, ratio_of_building_afn=0.85, 
-    window_scale=True, shading_scale=False,
+    window_scale=False, shading_scale=False,
     input_file= FILE_NAME, output_name='scaling_test.epJSON')
